b3e67d05e3805bc848141318fcd672f018cf42a0
daniellima123 <daniel.viegas.ds@gmail.com>
2023-09-13 09:30:02 -0300

    Criando o leitor dos indicadores
    Criando o Leitor dos indicadores e o requirements do projeto de dados

---
+++ analysis/data/indicadores.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from decouple import config
+
+
+class Indicadores:
+
+    def __init__(self):
+        self.url = config("DRIVE_FILE")
+        self.df_indicadores = pd.DataFrame(columns=['id', 'dimensao', 'componente'])
+        self.dict_indicadores = {}
+
+    def column_cleaner(self, col):
+        self.df_indicadores[col] = self.df_indicadores[col].str.replace(r'^[0-9\.\s\-]+', '', regex=True)
+
+    def reader(self):
+        self.df_indicadores = pd.read_csv(self.url)
+        self.df_indicadores = self.df_indicadores[['id_chave', 'indicador', 'dimensao', 'componente']]
+        self.df_indicadores.columns = ['id', 'nome', 'dimensao', 'topico']
+        for column in ['nome', 'dimensao', 'topico']:
+            self.column_cleaner(column)
+        self.dict_indicadores = self.df_indicadores.to_dict(orient='records')
+
+
+
